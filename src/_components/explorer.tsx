25b5bdb5ca73c451ae52047ac5645343bc865778
WMC <18013550018@163.com>
2025-08-12 13:03:11 +0800

    feat: init sidebar

--- src/_components/explorer.tsx
+++ src/_components/explorer.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { SidebarProvider, SidebarTrigger } from '@/_components/shadcn/ui/sidebar';
+
+import { AppSidebar } from './app-sidebar';
 import { ToolBar } from './toolbar';
 const Explorer = () => {
     return (
         <div className="msfiles floatTab dpShad" id={`ExplorerApp`} data-size="mini">
             <ToolBar app="EXPLORER" icon="explorer" name="File Explorer" size="full" />
-            <div className="windowScreen flex flex-col"></div>
+            <div className="windowScreen flex flex-col">
+                <SidebarProvider defaultOpen={true}>
+                    <AppSidebar />
+                    <main>
+                        <SidebarTrigger />
+                        <>fff</>
+                    </main>
+                </SidebarProvider>
+            </div>
         </div>
     );
 };
