3bded58a40f5420fd8693abaf7531e387c32b987
WMC <18013550018@163.com>
2025-09-09 22:44:18 +0800

    improve: dark&light mode loading png

--- src/_components/video-background.tsx
+++ src/_components/video-background.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const VideoBackground = () => {
+    const { theme } = useTheme();
+    const [videoUrl, setVideoUrl] = useState('');
+
     // const getBackgroundVideo = unstable_cache(
     //     async () => {
     //         const result = await fetchApi(async (c) =>
@@ -20,32 +22,20 @@ const VideoBackground = () => {
     //     { revalidate: 60 * 60 * 24 },
     // );
 
-    const { theme, setTheme } = useTheme();
-    const [videoUrl, setVideoUrl] = useState('');
     // if (process.env.NODE_ENV === 'production') {
     //     videoUrl = await getBackgroundVideo();
     // }
 
-    // const imgUrl = '/xqtd.png';
-    const [imgUrl, setImgUrl] = useState<string | null>(null);
-
     useEffect(() => {
-        setVideoUrl(theme === 'dark' ? '/test/xqtd.mp4' : '/test/xl.mp4');
-        setImgUrl(theme === 'dark' ? '/xqtd.png' : '/xl.png');
+        setVideoUrl(theme === 'dark' ? '/test/xqtd.webm' : '/test/xl.webm');
     }, [theme]);
 
     return (
         <div className="absolute inset-0 z-[-1] overflow-hidden w-full h-full min-h-full">
-            {imgUrl && (
-                <Image
-                    src={imgUrl}
-                    alt="xqtd.png"
-                    className="fixed top-0 left-0 object-cover h-full w-full z-[-2]"
-                    priority
-                    width={1920}
-                    height={1080}
-                ></Image>
-            )}
+            <div
+                className="fixed top-0 left-0 object-cover h-full w-full z-[-2] bg-[url('/xl.png')] dark:bg-[url('/xqtd.png')] bg-cover bg-center"
+                aria-hidden="true"
+            />
             {videoUrl && (
                 <video
                     key={videoUrl} // 添加key属性，当videoUrl改变时强制重新创建视频元素
