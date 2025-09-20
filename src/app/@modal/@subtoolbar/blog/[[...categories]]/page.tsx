387ec5e53e65cb0eebf1e02f9fc7bb9689916994
wmc <512111127@qq.com>
2025-09-20 21:44:19 +0800

    fix: subtoolbar出现在about, doc

--- src/app/@modal/@subtoolbar/blog/[[...categories]]/page.tsx
+++ src/app/@modal/@subtoolbar/blog/[[...categories]]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { CategoryWidget } from '../components/category-widget';
 
+// TODO: 在/blog/create关闭modal，再打开/blog会残留/blog/create的内容再刷新成/blog
 const BlogSubToolbar = () => {
     return <CategoryWidget />;
 };
